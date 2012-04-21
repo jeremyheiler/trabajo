d9752aa4ce1b4e62427f44b8e33593f0c0a2cfe3
jeremyheiler <jeremyheiler@gmail.com>
2012-04-20 21:51:31 -0400

    Back to the futures 3! (It's better than 2, I promise!)
    Instead of having multiple threads polling Redis, now there's one that polls all the queues and delegates work to the agent thread pool with futures. Also, promises are now used in order to manage the number of workers (and to minimize polling). The polling thread will block (by dereferencing a promise) until a work is available, and then begin polling with that worker.

--- src/trabajo/core.clj
+++ src/trabajo/core.clj
@@ -44,33 +44,90 @@
   [{:strs [func args] :as job}]
   (apply (ns-resolve *ns* (symbol func)) (read-string args)))
 
-(defn process-job
-  "Processes the job and handles any errors."
-  [job]
-  (try
-    (apply-job job)
-    (catch Exception e
-      (store-error job (.toString e)))))
+;(defn process-job
+;  "Processes the job and handles any errors."
+;  [job]
+;  (try
+;    (apply-job job)
+;    (catch Exception e
+;      (store-error job (.toString e)))))
 
-(def workers (ref {}))
+;(def workers (ref {}))
 
-(defn ^:private process [queue]
-  (loop []
-    (when-not (Thread/interrupted)
-      (if-let [job (dequeue queue)]
-        (process-job job)
-        (Thread/sleep 5000))
-      (recur))))
+;(defn ^:private process [queue]
+;  (loop []
+;    (when-not (Thread/interrupted)
+;      (if-let [job (dequeue queue)]
+;        (process-job job)
+;        (Thread/sleep 5000))
+;      (recur))))
 
-(defn work-on
-  "Returns a thread that polls the given queue until interrupted."
-  [queue]
-  (dosync
-    (let [t (Thread. #(process queue))]
-      (alter workers update-in [queue] #(conj (if (nil? %) [] %) t))
-      (.start t))))
+;(defn work-on
+;  "Returns a thread that polls the given queue until interrupted."
+;  [queue]
+;  (dosync
+;    (let [t (Thread. #(process queue))]
+;      (alter workers update-in [queue] #(conj (if (nil? %) [] %) t))
+;      (.start t))))
 
 (defn test-work [x]
   (with-open [out (java.io.FileWriter. "/home/jeremy/job.out" true)]
     (.write out (str x "\n"))))
+
+
+(def work-manager (ref {}))
+
+;(def poll-manager (ref {}))
+
+(defn ^:private request-promise-from-work-manager
+  ""
+  []
+  (dosync
+    (peek (:promises (alter work-manager update-in [:promises] conj (promise))))))
+
+(defn ^:private process-job
+  ""
+  [job]
+  (try
+    (apply-job job)
+    (catch Exception e (store-error job (.toString e)))
+    (finally (dosync
+               (let [p (peek (:promises @work-manager))]
+                 ;(alter work-manager update-in [:promises] #(-> % (pop) (conj (promise))))
+                 (alter work-manager update-in [:promises] pop)
+                 (deliver p true))))))
+
+(defn ^:private process
+  ""
+  [queues]
+  (loop [[q & qs :as q+qs] queues p (atom true)]
+    (when-not (Thread/interrupted)
+      (if (or
+            (< (count (:promises @work-manager)) (:max-workers @work-manager))
+            @p) ; blocks until a worker is available
+        (if-let [job (dequeue q)]
+          (let [f (future-call #(process-job job))]
+            ;(dosync (alter work-manager update-in [:futures] conj f))
+            (recur queues (request-promise-from-work-manager)))
+          (do
+            (Thread/sleep 5000)
+            (recur (if (nil? qs) queues qs) p)))
+        (recur q+qs (request-promise-from-work-manager))))))
+
+(defn start
+  "Starts a work manager that polls jobs on the given queue."
+  [n & queues]
+  (dosync
+    (if (nil? (:thread @work-manager))
+      (let [t (Thread. #(process queues))]
+        (alter work-manager assoc :thread t :max-workers n)
+        (.start t))
+      (throw (IllegalStateException. "A work manager has already been started.")))))
+
+(defn stop
+  "Stops the work manager if one has been started."
+  []
+  (dosync
+    (when-not (nil? (:thread @work-manager))
+      (.interrupt @work-manager))))
 
